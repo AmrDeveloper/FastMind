518e66373dfe6660a8ee9aa179028868dadb3157
AmrDeveloper <amr96@programmer.net>
2018-09-20 11:10:03 +0200

    Create : Android App Main and Login Activity xml without style

---
+++ FastMind - Android/app/src/main/java/com/amrdeveloper/fastmind/LoginActivity.java
@@ -0,0 +1,22 @@
+package com.amrdeveloper.fastmind;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+
+    public void goToMainActivity(View view){
+        Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+        startActivity(intent);
+    }
+}
