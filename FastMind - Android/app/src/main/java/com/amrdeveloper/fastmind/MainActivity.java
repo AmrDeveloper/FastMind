f8d12a3962c4b293190c7f837ba0397ee4a04635
AmrDeveloper <amr96@programmer.net>
2018-09-21 01:07:17 +0200

    Create : Android Multi and Single Player XML without design

--- FastMind - Android/app/src/main/java/com/amrdeveloper/fastmind/MainActivity.java
+++ FastMind - Android/app/src/main/java/com/amrdeveloper/fastmind/MainActivity.java
@@ -1,7 +1,9 @@
 package com.amrdeveloper.fastmind;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startSingleGame(View view){
+        // TODO : Make User Level = 0 , Score = 0
+        Intent intent = new Intent(this,SinglePlayActivity.class);
+        startActivity(intent);
+    }
+
+    public void continueSingleGame(View view){
+        Intent intent = new Intent(this,SinglePlayActivity.class);
+        startActivity(intent);
+    }
+
+    public void aboutActivity(View view){
+        Intent intent = new Intent(this,AboutActivity.class);
+        startActivity(intent);
     }
 }
