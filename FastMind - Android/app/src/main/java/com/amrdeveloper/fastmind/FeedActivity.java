a8b18f8abd10745689bbebbaaee878d56da412e3
AmrDeveloper <amr96@programmer.net>
2018-09-29 00:31:15 +0200

    Create : Feed Activity and Feed List item to show game history

---
+++ FastMind - Android/app/src/main/java/com/amrdeveloper/fastmind/FeedActivity.java
@@ -0,0 +1,13 @@
+package com.amrdeveloper.fastmind;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FeedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed);
+    }
+}
