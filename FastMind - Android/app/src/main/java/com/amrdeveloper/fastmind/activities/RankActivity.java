fc6c9985043e288f78a470c1764251f865aefbc4
AmrDeveloper <amr96@programmer.net>
2018-10-20 21:48:44 +0200

    Android : Implement Init Views on RankActivity and Create RankRecyclerAdapter

--- FastMind - Android/app/src/main/java/com/amrdeveloper/fastmind/activities/RankActivity.java
+++ FastMind - Android/app/src/main/java/com/amrdeveloper/fastmind/activities/RankActivity.java
@@ -2,14 +2,28 @@ package com.amrdeveloper.fastmind.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.widget.ProgressBar;
+
 import com.amrdeveloper.fastmind.R;
 
-
 public class RankActivity extends AppCompatActivity {
+
+    private ProgressBar mRankIndicator;
+    private RecyclerView mRankRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rank);
+        initializeViews();
+    }
+
+    /**
+     * Initialize All UI Views
+     */
+    private void initializeViews(){
+        mRankIndicator = findViewById(R.id.rankProgressBar);
+        mRankRecyclerView = findViewById(R.id.rankRecyclerView);
     }
 }
---
+++ FastMind - Android/app/src/main/java/com/amrdeveloper/fastmind/adapter/RankRecyclerAdapter.java
@@ -0,0 +1,68 @@
+package com.amrdeveloper.fastmind.adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.amrdeveloper.fastmind.Player;
+import com.amrdeveloper.fastmind.R;
+
+import java.util.List;
+
+public class RankRecyclerAdapter extends RecyclerView.Adapter<RankRecyclerAdapter.RankViewHolder>{
+
+    private List<Player> rankedPlayerList;
+
+    public RankRecyclerAdapter(List<Player> rankedPlayerList){
+        this.rankedPlayerList = rankedPlayerList;
+    }
+
+    @NonNull
+    @Override
+    public RankViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        int layoutIdForListItem = R.layout.rank_list_item;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        final boolean shouldAttachToParentImmediately = false;
+
+        View view = inflater.inflate(layoutIdForListItem, parent, shouldAttachToParentImmediately);
+
+        return new RankViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RankViewHolder holder, int position) {
+        Player currentPlayer = rankedPlayerList.get(position);
+        holder.bindView(currentPlayer);
+    }
+
+    @Override
+    public int getItemCount() {
+        return rankedPlayerList.size();
+    }
+
+    class RankViewHolder extends RecyclerView.ViewHolder{
+
+        private TextView mRankUserName;
+        private TextView mRankUserScore;
+
+        public RankViewHolder(View itemView) {
+            super(itemView);
+            initViews(itemView);
+        }
+
+        private void initViews(View view){
+            mRankUserName = view.findViewById(R.id.rankUserTxt);
+            mRankUserScore = view.findViewById(R.id.rankScoreTxt);
+        }
+
+        private void bindView(Player player){
+            mRankUserName.setText(player.getUsername());
+            mRankUserScore.setText(String.valueOf(player.getScore()));
+        }
+    }
+}
