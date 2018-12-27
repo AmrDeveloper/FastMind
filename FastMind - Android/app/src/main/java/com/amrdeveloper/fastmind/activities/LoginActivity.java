ccc188e1148e599ba793c3f41c0d44f16008328d
AmrDeveloper <amr96@programmer.net>
2018-12-27 15:41:31 +0200

    Android : Login Test Validation and create Login DEMO

--- FastMind - Android/app/src/main/java/com/amrdeveloper/fastmind/activities/LoginActivity.java
+++ FastMind - Android/app/src/main/java/com/amrdeveloper/fastmind/activities/LoginActivity.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import com.amrdeveloper.fastmind.R;
+import com.amrdeveloper.fastmind.utils.DataValidation;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -29,7 +30,7 @@ public class LoginActivity extends AppCompatActivity {
         initializeViews();
     }
 
-    private void initializeViews(){
+    private void initializeViews() {
         mEmailInputLayout = findViewById(R.id.emailInputLayout);
         mEmailEditText = findViewById(R.id.emailEditText);
 
@@ -37,17 +38,37 @@ public class LoginActivity extends AppCompatActivity {
         mPassWordEditText = findViewById(R.id.passWordEditText);
 
         mLoginButton = findViewById(R.id.loginButton);
+        mLoginButton.setOnClickListener(onLoginClickListener);
     }
 
-    public void goToSigninActivity(View view){
-        Intent intent = new Intent(LoginActivity.this,SigninActivity.class);
+    public void goToSigninActivity(View view) {
+        Intent intent = new Intent(LoginActivity.this, SigninActivity.class);
         startActivity(intent);
         finish();
     }
 
-    public void goToMainActivity(View view){
-        Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+    public void goToMainActivity() {
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
         startActivity(intent);
         finish();
     }
+
+    private View.OnClickListener onLoginClickListener = view -> {
+        String playerEmail = mEmailEditText.getText().toString().trim();
+        String playerPassword = mPassWordEditText.getText().toString().trim();
+
+        boolean emailValid = DataValidation.isEmailValid(playerEmail);
+        boolean passValid = DataValidation.isPasswordValid(playerPassword);
+
+        if(emailValid && passValid){
+            goToMainActivity();
+        }else{
+            if(!emailValid){
+                mEmailInputLayout.setError(getString(R.string.invalid_Email));
+            }
+            if(!passValid){
+                mPassWordInputLayout.setError(getString(R.string.invalid_password));
+            }
+        }
+    };
 }
