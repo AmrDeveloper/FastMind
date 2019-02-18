1bd5d2aa979cfd8a56a65f01abe15f089bea1e73
AmrDeveloper <Amr96@programmer.net>
2019-02-18 09:31:48 +0200

    DataBinding Beta on Settings Activity

--- FastMind - Android/app/src/main/java/com/amrdeveloper/fastmind/activities/SettingsActivity.java
+++ FastMind - Android/app/src/main/java/com/amrdeveloper/fastmind/activities/SettingsActivity.java
@@ -1,14 +1,18 @@
 package com.amrdeveloper.fastmind.activities;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import com.amrdeveloper.fastmind.R;
+import com.amrdeveloper.fastmind.databinding.ActivitySettingsBinding;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    private ActivitySettingsBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_settings);
     }
 }
